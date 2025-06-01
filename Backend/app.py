3f741bf959e6c17fa599e7c11de199222f405e13
Abonkwe <abonkweprincely2005@gmail.com>
2025-06-01 07:07:36 +0100

    WIP: local changes to app.py

--- Backend/app.py
+++ Backend/app.py
@@ -273,4 +273,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True, port=5002)
+    # app.run(debug=True, port=5002)
+    app.run(host="0.0.0.0", port=5000)
